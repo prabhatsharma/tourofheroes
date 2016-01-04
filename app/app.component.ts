2cbee734a386f2c3bfbd67fe6ca79be3c91786a8
Prabhat Sharma <hi.prabhat@gmail.com>
2016-01-04 10:43:18 +0530

    Refactored the code and completed chapter4. Using promises now

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,35 @@
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 import {HeroDetailComponent} from './hero-detail.component';
+import {OnInit} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     styleUrls : ['app/app.component.css'],
-    directives: [HeroDetailComponent]    
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private _heroService : HeroService) {
+        
+    }
+    
+    getHeroes() {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    
+    ngOnInit() {
+        this.getHeroes();
+    }
+    
+    
     public title = 'Tour of Heroes';
 
     public selectedHero: Hero ;
 
-    public heroes = HEROES;
+    public heroes : Hero[];
     
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
